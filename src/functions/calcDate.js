02fa838cca823a31acb18de3807b82e70ef07f85
Diego Arruda <arrudadiego45@gmail.com>
2022-10-13 12:16:00 -0300

    Fix NaN returns in ios

--- src/functions/calcDate.js
+++ src/functions/calcDate.js
@@ -1,6 +1,6 @@
 export default function calcDate() {
   const currentDate = new Date();
-  const openingDate = new Date('2022-10-31 23:59');
+  const openingDate = new Date(2022, 9, 31, 23, 59, 0);
 
   const remainingDays = (Math.abs(openingDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
   const days = Math.floor(remainingDays);
