b6a842d4754838a45dadb7f1822957ce0fe14896
Diego Arruda <arrudadiego45@gmail.com>
2022-10-12 23:27:37 -0300

    Changing the final date

--- src/functions/calcDate.js
+++ src/functions/calcDate.js
@@ -1,6 +1,6 @@
 export default function calcDate() {
   const currentDate = new Date();
-  const openingDate = new Date('2022-11-16 15:30');
+  const openingDate = new Date('2022-10-31 23:59');
 
   const remainingDays = (Math.abs(openingDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
   const days = Math.floor(remainingDays);
