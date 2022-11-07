6893f589e46103865820dbc6e31792ffcaa2488d
Diego Arruda <arrudadiego45@gmail.com>
2022-11-07 10:33:12 -0300

    Change date

--- src/functions/calcDate.js
+++ src/functions/calcDate.js
@@ -1,6 +1,6 @@
 export default function calcDate() {
   const currentDate = new Date();
-  const openingDate = new Date(2022, 9, 31, 23, 59, 0);
+  const openingDate = new Date(2023, 9, 31, 23, 59, 0);
 
   const remainingDays = (Math.abs(openingDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
   const days = Math.floor(remainingDays);
